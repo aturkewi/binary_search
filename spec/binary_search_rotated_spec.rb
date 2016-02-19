da4ef8902136c101c1a74506c89f174cb2e320c8
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-02-19 09:07:55 -0500

    Add test for pivot in right half of array

--- spec/binary_search_rotated_spec.rb
+++ spec/binary_search_rotated_spec.rb
@@ -3,9 +3,14 @@ require_relative 'spec_helper'
 
 describe "rotated binary sort methods" do
   let(:array) {[11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+  let(:array_right) {[3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2]}
   describe "find_pivot" do
-    it 'correctly returns pivot location in array' do
+    it 'correctly returns pivot location in left half of array' do
       expect(find_pivot(array)).to eq(2)
+    end
+
+    it 'correctly finds pivot location in right half of array' do
+      expect(find_pivot(array_right)).to eq(10)
     end
 
     it "returns 'No  pivot' if the array is not rotated" do
